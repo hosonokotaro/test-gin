ec989de67bde72fe1a8dc1e825b001bc7e4c9b42
hosonokotaro <hosono1985@gmail.com>
2022-07-03 19:18:38 +0900

    feat: Add post method

--- main.go
+++ main.go
@@ -6,12 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Login struct {
+	User     string `json:"user" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "test",
-		})
+
+	// NOTE: router に必要な設定を入れる
+	router.POST("/login", func(ctx *gin.Context) {
+		var json Login
+		if err := ctx.ShouldBindJSON(&json); err != nil {
+			// NOTE: err が詳細に鳴くと、攻撃者に仕組みを類推されるおそれがある
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "manu" || json.Password != "123" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
-	router.Run()
+
+	// NOTE: router を起動する
+	router.Run(":8080")
 }
