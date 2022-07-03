bf6a7fcbe5546de7a4471968e2b9b40f50df7c4c
hosonokotaro <hosono1985@gmail.com>
2022-07-03 18:20:04 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "test",
+		})
+	})
+	router.Run()
+}
